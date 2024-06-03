a22760ebe24918db0e8df221925c1ee42d0b5621
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-06-03 22:13:54 +0800

    以二进制文件方式传输给客户端png图片

---
+++ http_server.c
@@ -0,0 +1,102 @@
+#include <func.h>
+#include <asm-generic/socket.h>
+
+//未部署服务器，只能通过传输二进制文件的方式，给浏览器传输图片等文件
+
+int main(void)
+{
+    //创建套接字
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(listenfd == -1){
+        error(1, errno, "socket");
+    }
+    
+    //填充网络地址
+    struct sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(8080);
+    serveraddr.sin_addr.s_addr = inet_addr("192.168.248.136");
+    
+    //设置网络地址可重用
+    int on = 1;
+    int ret = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    ERROR_CHECK(ret , 1 ,"setsockopt");
+
+    //绑定IP地址和端口
+    ret = bind(listenfd, (const struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    if(ret == -1){
+        error(1, errno, "bind");
+    }
+
+    //监听
+    ret = listen(listenfd, 10);
+    if(ret == -1){
+        error(1, errno, "listen");
+    }
+
+    while(1){
+        struct sockaddr_in clientAddr;
+        memset(&clientAddr, 0, sizeof(clientAddr));
+        socklen_t len = sizeof(clientAddr);
+        int peerfd = accept(listenfd, (struct sockaddr*)&clientAddr, &len);
+        printf("%s:%d has connected.\n",inet_ntoa(clientAddr.sin_addr),ntohs(clientAddr.sin_port));
+        
+
+        //读取图片文件
+        FILE *png_file = fopen("1.png","rb");
+        if(png_file == NULL){
+            perror("fopen");
+            exit(1);
+        }       
+        //获取文件大小
+        fseek(png_file, 0, SEEK_END);
+        long filesize = ftell(png_file);
+        fseek(png_file, 0, SEEK_SET);
+
+        //读取文件内容
+        char* png_file_buff = (char*)malloc(filesize);
+        fread(png_file_buff, 1, filesize, png_file);
+        fclose(png_file);
+
+        //获取http请求报文
+        char buff[4096] = {0};
+        ret = recv(peerfd, buff, sizeof(buff), 0);
+        printf("ret: %d bytes.\n%s\n",ret, buff);
+
+        //对请求报文进行响应操作
+        const char* startLine = "HTTP/1.1 200 OK\r\n";
+        const char* headers = "Server: MyHttpServer1.0\r\n"
+                "Content-Type: image/png\r\n"
+                /* "Content-Type: text/html\r\n" */
+                "Content-Length: ";
+        const char* emptyLine = "\r\n";
+        /* const char* body = */ 
+        /*     "<html>" */
+        /*     "   <head>Test Image</head>" */
+        /*     "   <body>" */
+        /*             "<img src=\"1.png\" width=\"100%\">" */
+        /*     "   </body>" */
+        /*     "</html>"; */
+        
+        //清空缓冲区
+        memset(buff, 0 ,sizeof(buff));
+        /* sprintf(buff, "%s%s%ld\r\n%s%s", startLine, headers, strlen(body), emptyLine, body); */
+        sprintf(buff, "%s%s%ld\r\n%s", startLine, headers, filesize, emptyLine);
+        printf("response:\r\n%s\n", buff);
+        
+        //发送响应头
+        ret = send(peerfd, buff, strlen(buff), 0);
+        printf("\nsend %d bytes.\n", ret);
+
+        //发送图片内容:
+        send(peerfd, png_file_buff, filesize, 0);
+
+        free(png_file_buff);
+        close(peerfd);
+    }
+
+    close(listenfd);
+
+    return 0;
+}
